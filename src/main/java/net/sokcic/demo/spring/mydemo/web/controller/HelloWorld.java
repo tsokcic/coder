af3b50a8c39ce22b23d62fda82a4bd46c4936131
Your Name <you@example.com>
2022-11-26 11:46:22 +0100

    update

--- src/main/java/net/sokcic/demo/spring/mydemo/web/controller/HelloWorld.java
+++ src/main/java/net/sokcic/demo/spring/mydemo/web/controller/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 
     @GetMapping(value="/")
     public ResponseEntity<?> helloWorld() {
-        return ResponseEntity.ok("Hello World 123");
+        return ResponseEntity.ok("Hello World 123-456");
     }
     
     
